9bb1aa825917eaea302f86474606dd89fec7f404
Gajo Petrovic <gajo01@gmail.com>
2018-03-08 17:14:27 +0900

    fix modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -3,9 +3,8 @@ local modinfo = {
 	shortname = "Parts Needed",
 	game = "Parts Needed",
 	shortgame = "Parts Needed",
-    version = "1.1",
+	version = "$VERSION",
 	description = "Ludum Dare 35 entry",
-	
 	modtype = "1",
 	depend = {
 		"cursors.sdz",
